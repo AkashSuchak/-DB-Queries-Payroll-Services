01bcdff5bd877e10d57e1463d0d8c79d2571a64a
AkashSuchak <72850833+AkashSuchak@users.noreply.github.com>
2021-09-06 23:34:50 +0530

    feat: SQL : Retrieve All Data from Table

--- payroll_services.sql
+++ payroll_services.sql
@@ -20,3 +20,8 @@ insert into employee_payroll(Name, Salary, Start) VALUES
 	('Billi',100000.0,'2018-01-03'),
 	('Terisa',200000.0,'2019-11-13'),
 	('Charlie',300000.0,'2021-05-21')
+
+--UC4 Retrieving Data
+use payroll_service
+
+select * from employee_payroll
