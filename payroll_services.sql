986c7c2b78c835446c97fa647e5f2441872e82ee
AkashSuchak <72850833+AkashSuchak@users.noreply.github.com>
2021-09-06 23:28:17 +0530

    feat: SQL: Create Table

--- payroll_services.sql
+++ payroll_services.sql
@@ -4,3 +4,11 @@
 create Database payroll_service
 select * from sys.databases
 use payroll_service
+
+--UC2 Create Table employee_payroll in Payroll Service
+Create table employee_payroll	(
+	Id int identity(1, 1) primary Key,
+	Name varchar(50),
+	Salary money Not Null,
+	Start datetime Not Null
+	)
